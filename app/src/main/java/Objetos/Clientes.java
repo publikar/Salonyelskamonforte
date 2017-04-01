ca65f49dd4913d2780b9f740ee15427d1f9f2b67
toluam <ana.toledo@cenat.com.mx>
2017-04-01 12:19:35 -0600

    objeto clientes

---
+++ app/src/main/java/Objetos/Clientes.java
@@ -0,0 +1,72 @@
+package Objetos;
+
+/**
+ * Created by A on 01/04/2017.
+ */
+
+public class Clientes {
+
+    private int idclientes;
+    private String nombre;
+    private String apellidos;
+    private String movil;
+    private String email;
+    private String cumpleaños;
+    private String password;
+
+    public int getIdclientes() {
+        return idclientes;
+    }
+
+    public void setIdclientes(int idclientes) {
+        this.idclientes = idclientes;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getMovil() {
+        return movil;
+    }
+
+    public void setMovil(String movil) {
+        this.movil = movil;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCumpleaños() {
+        return cumpleaños;
+    }
+
+    public void setCumpleaños(String cumpleaños) {
+        this.cumpleaños = cumpleaños;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
