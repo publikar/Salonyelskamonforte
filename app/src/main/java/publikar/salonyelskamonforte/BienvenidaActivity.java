a5f459f3f45e876ea10f5d7e68def7f88af5f557
toluam <ana.toledo@cenat.com.mx>
2017-04-08 10:18:39 -0500

    permisos para marcar un telefono

--- app/src/main/java/publikar/salonyelskamonforte/BienvenidaActivity.java
+++ app/src/main/java/publikar/salonyelskamonforte/BienvenidaActivity.java
@@ -1,6 +1,11 @@
 package publikar.salonyelskamonforte;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +15,13 @@ public class BienvenidaActivity extends AppCompatActivity {
 
     Button btnregistrar;
     Button btnlogin;
+    Button btnllamar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenida);
-
+        btnllamar = (Button) findViewById(R.id.buttonLlama);
         btnregistrar = (Button) findViewById(R.id.btnInscribete);
         btnregistrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,19 +33,28 @@ public class BienvenidaActivity extends AppCompatActivity {
             }
         });
 
-       //Intent intent = new Intent(this, RegistroActivity.class);
+        btnllamar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+requestpermission();
+            }
+        });
+
+
+        //Intent intent = new Intent(this, RegistroActivity.class);
 
         //{
-            btnlogin = (Button) findViewById(R.id.btnIngresa);
-            btnlogin.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intentLog = new Intent(BienvenidaActivity.this, LoginActivity.class);
-                    startActivity(intentLog);
+        btnlogin = (Button) findViewById(R.id.btnIngresa);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLog = new Intent(BienvenidaActivity.this, LoginActivity.class);
+                startActivity(intentLog);
 
 
-                }
-            });
+            }
+        });
+    }
 
           //  Intent intentLog = new Intent(this, LoginActivity.class);
 
@@ -47,7 +63,25 @@ public class BienvenidaActivity extends AppCompatActivity {
 
         //}
 
+        private void requestpermission() {
 
+            ActivityCompat.requestPermissions(BienvenidaActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    0);
+        }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:999811007"));
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                startActivity(intent);
+            }
+
+        }
     }
 }
