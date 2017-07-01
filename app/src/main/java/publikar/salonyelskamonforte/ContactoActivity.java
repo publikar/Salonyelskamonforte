50a9a3d42dfbd20730fd6300bc6246ca0c5110b1
toluam <ana.toledo@cenat.com.mx>
2017-07-01 11:02:32 -0500

    text size dimensions

--- app/src/main/java/publikar/salonyelskamonforte/ContactoActivity.java
+++ app/src/main/java/publikar/salonyelskamonforte/ContactoActivity.java
@@ -2,12 +2,17 @@ package publikar.salonyelskamonforte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.TextView;
 
 public class ContactoActivity extends AppCompatActivity {
-
+TextView txtfacebook;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+        txtfacebook=(TextView)findViewById(R.id.txtface);
+        txtfacebook.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                getResources().getDimension(R.dimen.textsize));
     }
 }
